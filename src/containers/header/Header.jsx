38947d1e01458cab391b8835b4d278130332c80f
Jacob Foggie <jacob.foggie@outlook.com>
2024-07-07 15:43:58 +0100

    Add Header styling

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -19,9 +19,9 @@ const Header = () => {
           <p>1,600 people requested access to visit in the last 24 hours</p>
         </div>
 
-        {/* <div className="gpt3__header-image">
-          <img src={ai} alt="ai" />
-        </div> */}
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="ai" />
       </div>
     </div>
   )
