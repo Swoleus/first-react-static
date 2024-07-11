09ecf68a94c849ee088c70e90061463d7d5a7719
Jacob Foggie <jacob.foggie@outlook.com>
2024-07-11 15:14:37 +0100

    create imports.js in blog container folder, to allow blog img links from assets add randomDate function to generate random dates on blog container, to pass through Article date call finish article.jsx and styling of article component

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { Article } from '../../components';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports';
 import './blog.css';
+
+const randomDate = getRandomDate(new Date(2000, 0, 1), new Date(2024, 11, 30))
+
+
+
+function getRandomDate(start, end) {
+  const date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  return date;
+}
 
 const Blog = () => {
   return (
@@ -10,17 +20,22 @@ const Blog = () => {
       </div>
       <div className="gpt3__blog-container">
         <div className="gpt3__blog-container_groupA">
-          <Article />
+          <Article imgUrl={blog01} date={randomDate.toLocaleDateString()} title="GPT3 is the future, let's see where it can take us!"/>
         </div>
         <div className="gpt3__blog-container_groupB">
-          <Article />
-          <Article />
-          <Article />
-          <Article />
+          <Article imgUrl={blog02} date={randomDate.toLocaleDateString()} title="GPT3 is the future, let's see where it can take us!"/>
+          <Article imgUrl={blog03} date={randomDate.toLocaleDateString()} title="GPT3 is the future, let's see where it can take us!"/>
+          <Article imgUrl={blog04} date={randomDate.toLocaleDateString()} title="GPT3 is the future, let's see where it can take us!"/>
+          <Article imgUrl={blog05} date={randomDate.toLocaleDateString()} title="GPT3 is the future, let's see where it can take us!"/>
         </div>
       </div>
     </div>
   )
 }
+
+// function getRandomDate(start, end) {
+//   const date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+//   return date;
+// }
 
 export default Blog
